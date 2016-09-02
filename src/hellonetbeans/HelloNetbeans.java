a0c5285b9e5e46a1062071b0a7de391011348ab6
Phil O'Connell <github.amgine@xoxy.net>
2016-09-02 00:21:43 -0400

    Added more excitement!!

--- src/hellonetbeans/HelloNetbeans.java
+++ src/hellonetbeans/HelloNetbeans.java
@@ -15,7 +15,7 @@ public class HelloNetbeans {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello, Netbeans!");
+    System.out.println("Hello, Netbeans!!!!");
   }
   
 }
