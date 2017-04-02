006bb7e86b2ce2f2e161ef96052c94da951a109c
phil <pxo4@psu.edu>
2017-04-02 01:37:29 -0400

    Restored main class

---
+++ src/hellonetbeans/HelloNetbeans.java
@@ -0,0 +1,15 @@
+package hellonetbeans;
+
+/**
+ * @author Phil O'Connell <pxo4@psu.edu>
+ */
+public class HelloNetbeans {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Netbeans!!!!");
+	}
+
+}
